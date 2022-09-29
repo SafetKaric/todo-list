dac0ae925c8b44f2f954af0285270398057d04f0
safetkaric <safetkaric247@gmail.com>
2022-09-29 22:29:05 +0200

    firebase

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+    return (
+        <div className="">
+            <header>
+                <div className="content">
+                    <a href="/">
+                        <h1>Todo App</h1>
+                    </a>
+                    <a href="/">
+                        <p>github.com</p>
+                    </a>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBI0TDNkaqgDSAugQjI-TV2EDjS5y2b8Po",
+    authDomain: "todo-list-e9fab.firebaseapp.com",
+    projectId: "todo-list-e9fab",
+    storageBucket: "todo-list-e9fab.appspot.com",
+    messagingSenderId: "162810527198",
+    appId: "1:162810527198:web:353cfee8065fd3c6a5714f",
+};
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+const collectionRef = collection(db, "todos");
+
+getDocs(collectionRef).then((item) => {
+    item.docs.map((item) => console.log(item.data()));
+});
+
+// addDoc(collectionRef, {
+//     id: Math.random(),
+//     title: "This is from code",
+//     completed: false,
+//     time: Date.now().toLocaleString(),
+// }).then((e) => console.log(e));
