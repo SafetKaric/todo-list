2435eb1ee851e9a2c620c74a33d15ea3891db1ca
SafetKaric <77508133+SafetKaric@users.noreply.github.com>
2022-10-06 00:14:08 +0200

    Update firebase.config.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBI0TDNkaqgDSAugQjI-TV2EDjS5y2b8Po",
-    authDomain: "todo-list-e9fab.firebaseapp.com",
-    projectId: "todo-list-e9fab",
-    storageBucket: "todo-list-e9fab.appspot.com",
-    messagingSenderId: "162810527198",
-    appId: "1:162810527198:web:353cfee8065fd3c6a5714f",
-};
-
 initializeApp(firebaseConfig);
 
 export const db = getFirestore();
